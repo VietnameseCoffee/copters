cd4f9b53e6c5fda045591e4c7277e91e98ba1557
Nate <nathanharris.sf@gmail.com>
2018-08-21 10:56:30 -0700

    Have heli and circ stay in the same frame

--- javascript/copters.js
+++ javascript/copters.js
@@ -45,7 +45,32 @@ c.stroke();
 let i = 0;
 
 const animate = () => {
-  console.log(i);
+
+  c.clearRect(0,0, 900, 600);
+  // Copter
+
+  c.beginPath();
+  c.ellipse(200, 200, 40, 10, 0, 0, 2*Math.PI);
+  c.stroke();
+
+  c.beginPath();
+  c.strokeRect(175, 205, 50, 25);
+  c.stroke();
+
+  c.beginPath();
+  c.strokeRect(170, 233, 65, 3);
+  c.stroke();
+
+  c.beginPath();
+  c.strokeRect(145, 210, 30, 7);
+  c.stroke();
+
+  c.beginPath();
+  c.arc(145,210,6,0,2*Math.PI);
+  c.stroke();
+
+
+  // circle
   c.beginPath();
   c.arc(400 + i, 200 , 50, 0, 2*Math.PI);
   c.stroke();
@@ -53,4 +78,4 @@ const animate = () => {
   requestAnimationFrame(animate);
 };
 
-// animate();
+animate();
