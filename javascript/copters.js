c9a87fc5214836df322fd9c0626f9cb7e493d05a
Nate <nathanharris.sf@gmail.com>
2018-08-21 10:32:10 -0700

    Draw an copter

--- javascript/copters.js
+++ javascript/copters.js
@@ -9,17 +9,28 @@ canvas.height = 640;
 
 const c = canvas.getContext('2d');
 
-c.beginPath();
-c.moveTo(0,0)
-c.lineTo(50,50);
-c.stroke();
+
 
 c.beginPath();
 c.arc(100,75,50,0,2*Math.PI);
 c.stroke();
 
 c.beginPath();
-c.ellipse(200, 200, 50, 10, 0, 0, 2*Math.PI);
+c.ellipse(200, 200, 40, 10, 0, 0, 2*Math.PI);
 c.stroke();
 
-c.clearRect(0, 0, 100, 100);
+c.beginPath();
+c.strokeRect(175, 205, 50, 25);
+c.stroke();
+
+c.beginPath();
+c.strokeRect(170, 233, 65, 3);
+c.stroke();
+
+c.beginPath();
+c.strokeRect(145, 210, 30, 7);
+c.stroke();
+
+c.beginPath();
+c.arc(145,210,6,0,2*Math.PI);
+c.stroke();
