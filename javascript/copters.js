fe56d7488e7e35f0d9bfb9e3c96200b81475235e
Nate <nathanharris.sf@gmail.com>
2018-08-21 18:05:15 -0700

    Import logics into game class

--- javascript/copters.js
+++ javascript/copters.js
@@ -6,40 +6,45 @@ canvas.width = 1000;
 canvas.height = 640;
 const c = canvas.getContext('2d');
 
-let copter = new Helicopter(150, 150, 1);
-let brick = new Brick(1000, 200, 3)
-
-const animate = () => {
-  c.clearRect(0,0, 1000, 640);
-
-  copter.move(c);
-  brick.move(c);
-
-  requestAnimationFrame(animate);
-};
-animate();
+// let copter = new Helicopter(150, 150, 1);
+// let brick = new Brick(1000, 200, 3)
+//
+// const animate = () => {
+//   c.clearRect(0,0, 1000, 640);
+//
+//   copter.move(c);
+//   brick.move(c);
+//
+//   requestAnimationFrame(animate);
+// };
+// animate();
 
 
 class CoptersGame {
 
   constructor(c) {
-    this.copter = new Helicopter(150, 150, 1)
+    this.copter = new Helicopter(150, 150, 1);
     this.c = c;
+    this.brick = new Brick(1000, 200, 3);
+
+    this.animate = this.animate.bind(this);
   }
 
   play() {
-
 
   }
 
   animate() {
     this.c.clearRect(0,0, 1000, 640);
 
-    copter.move(this.c);
-    brick.move(this.c);
+    this.copter.move(this.c);
+    this.brick.move(this.c);
 
-    requestAnimationFrame(animate);
+    requestAnimationFrame(this.animate);
   }
 
-
 }
+
+let g = new CoptersGame(c);
+
+g.animate();
