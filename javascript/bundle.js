d13c2cbfc9f8b968aef3b0eb41f6c8e32c275985
Kevin <kevin.ou705@gmail.com>
2018-09-24 14:30:56 -0700

    Add animation for the sprite image

--- javascript/bundle.js
+++ javascript/bundle.js
@@ -501,62 +501,33 @@ class Helicopter extends _collidable__WEBPACK_IMPORTED_MODULE_0__["default"] {
     let img = new Image();
     img.src = "https://raw.githubusercontent.com/VietnameseCoffee/copters/master/imports/helicopter_sprites.png"
 
-    this.width = 90;
-    this.height = 47;
+    this.width = 160;
+    this.height = 68;
     this.g = 0.33;
     this.img = img
+    this.frame = 0;
+    this.tick = 0;
 
     this.draw = this.draw.bind(this);
     this.move = this.move.bind(this);
-    this.frame = 0;
-    this.tick = 0;
+    this.update = this.update.bind(this);
   }
 
   draw(c) {
+    this.update();
 
-    c.drawImage(this.img, 0, 0, 423, 150, this.x, this.y, 90, 47)
-    // fuselage
-    // c.beginPath();
-    // c.fillStyle = "#697777";
-    // c.fillRect((this.x+30),(this.y+15), 48, 25);
-    // c.stroke();
-    // c.beginPath();
-    // c.strokeRect((this.x+30),(this.y+15), 48, 25);
-    // c.stroke();
-    //
-    // // landing skids
-    // c.beginPath();
-    // c.fillStyle = "#697777";
-    // c.fillRect((this.x+24), (this.y+43), 65, 3);
-    // c.fill();
-    // c.stroke();
-    // c.beginPath();
-    // c.strokeRect((this.x+24), (this.y+43), 65, 3);
-    // c.stroke();
-    // // tail boom
-    // c.beginPath();
-    // c.fillStyle = "#697777";
-    // c.fillRect((this.x+8), (this.y+26), 22, 7);
-    // c.stroke();
-    // c.beginPath();
-    // c.strokeRect((this.x+8), (this.y+26), 22, 7);
-    // c.stroke();
-    // // steering blades
-    // c.beginPath();
-    // c.arc((this.x+8), (this.y+26),6,0,2*Math.PI);
-    // c.fillStyle = "#5dd8d8";
-    // c.fill();
-    // c.stroke();
-    // // blades
-    // c.beginPath();
-    // c.ellipse((this.x + 52), (this.y + 12), 40, 10, 0, 0, 2*Math.PI);
-    // c.fillStyle = "#5dd8d8";
-    // c.fill();
-    // c.stroke();
-    // // box
-    // c.beginPath();
-    // c.strokeRect((this.x), (this.y), 92, 47);
-    // c.stroke();
+    c.drawImage(this.img, 0, (this.frame * 150), 400, 150, this.x, this.y, this.width, this.height)
+  }
+
+  update () {
+    this.tick = this.tick + 1;
+    if (this.tick === 8) {
+      this.tick = 0;
+      this.frame = (this.frame + 1);
+      if (this.frame === 4) {
+        this.frame = 0
+      }
+    }
   }
 
   move(c) {
--- javascript/helicopter.js
+++ javascript/helicopter.js
@@ -9,27 +9,33 @@ class Helicopter extends Collidable {
     let img = new Image();
     img.src = "https://raw.githubusercontent.com/VietnameseCoffee/copters/master/imports/helicopter_sprites.png"
 
-    this.width = 90;
-    this.height = 47;
+    this.width = 160;
+    this.height = 68;
     this.g = 0.33;
     this.img = img
+    this.frame = 0;
+    this.tick = 0;
 
     this.draw = this.draw.bind(this);
     this.move = this.move.bind(this);
-    this.frame = 0;
-    this.tick = 0;
+    this.update = this.update.bind(this);
   }
 
   draw(c) {
-    this.tick = this.tick += 1;
-
     this.update();
 
-    c.drawImage(this.img, 0, 0, 423, 150, this.x, this.y, 90, 47)
+    c.drawImage(this.img, 0, (this.frame * 150), 400, 150, this.x, this.y, this.width, this.height)
   }
 
   update () {
-    
+    this.tick = this.tick + 1;
+    if (this.tick === 8) {
+      this.tick = 0;
+      this.frame = (this.frame + 1);
+      if (this.frame === 4) {
+        this.frame = 0
+      }
+    }
   }
 
   move(c) {
